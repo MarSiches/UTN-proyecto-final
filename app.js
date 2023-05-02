71997ba6e9a4fbf72a94ce4570b65ebb0954af69
MaruSiches <marinaisiches@gmail.com>
2023-05-02 14:04:33 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express()
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.listen(3050, () => {console.log("El servidor esta funcionando http://localhost:3050/")});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/home.html'))
+});
+app.get('/historia', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/historia.html'))
+})
+app.get('/poneraprueba', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/poneraprueba.html'))
+})
